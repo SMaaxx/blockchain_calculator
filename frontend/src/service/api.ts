0c9c4a9932a4d3006d4dc6eaa9062aa85013c274
Max <m.kostarev2004@gmail.com>
2024-06-10 14:16:29 +0500

    feat: error handler added

--- frontend/src/service/api.ts
+++ frontend/src/service/api.ts
@@ -4,8 +4,13 @@ export const calculate = async (expression: string) => {
   try {
     const response = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/api/calculation`, { expression: expression });
 
+
+    console.log('response.data -> ', response.data);
     return response.data
   } catch (e) {
-    console.log('Cannot get', e);
+    return {
+      history: ['Ошибка'],
+      result: 'Ошибка'
+    }
   }
 };
